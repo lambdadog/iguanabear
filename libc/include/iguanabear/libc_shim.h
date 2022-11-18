8e483db53f2177800ef95bf8e2fdafe50fc443bd
Ashlynn Anderson <pea@pea.sh>
2022-11-18 15:03:09 -0500

    Clean up iguanabear/libc_shim.h

--- libc/include/iguanabear/libc_shim.h
+++ libc/include/iguanabear/libc_shim.h
@@ -1,10 +1,10 @@
-// FIXME: figure out a better way to do this. Ideally it could just
-// use Zig's usize somehow... not sure how though
 #ifndef IGUANABEAR_LIBC_SHIM
 #define IGUANABEAR_LIBC_SHIM
 
 /* ----- */
 
+// FIXME: figure out a better way to do this. Ideally it could just
+// use Zig's usize somehow... not sure how though
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 typedef long long ssize_t;
 typedef unsigned long long size_t;
@@ -12,6 +12,8 @@ typedef unsigned long long size_t;
 typedef long ssize_t;
 typedef unsigned long size_t;
 #endif // WIN32
+
+/* ----- */
 
 void *memcpy(void *s1, const void *s2, size_t n);
 void *memset(void *s, int c, size_t n);
@@ -21,14 +23,10 @@ size_t strlen(const char *s);
 
 /* ----- */
 
-
-
-/* ----- */
-
 typedef size_t time_t;
 
 time_t time(time_t *t);
 
 /* ----- */
 
-#endif // IGUANABEAR_LIBC
+#endif // IGUANABEAR_LIBC_SHIM
